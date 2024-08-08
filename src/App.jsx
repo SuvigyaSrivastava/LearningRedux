3275ce888818b152bec7567d792ef35abd0f6802
Suvigya Srivastava <suvigyasrvstva@gmail.com>
2024-08-09 03:08:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes } from "react-router-dom";
+// import Navbar from "./components/Navbar"
+import { Route } from "react-router-dom";
+// import Home from "./pages/Home"
+// import Cart from "./pages/Cart"
+import Counter from "./components/Counter";
+
+const App = () => {
+  return (<div>
+    <Counter />
+  </div>)
+};
+
+export default App;
